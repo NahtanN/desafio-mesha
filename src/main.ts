288e0d7444c43cb7f3dccb9605cf8311074492c5
NahtanN <nahtann@outlook.com>
2022-10-31 16:15:47 -0300

    feat(response): global Filter para as respostas da API adicionado

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/exception.filter';
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefixer = 'api';
+
+  app.enableCors();
+  app.setGlobalPrefix(globalPrefixer);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(port, () =>
+    Logger.log(`Listening at http://localhost:${port}/${globalPrefixer}`),
+  );
 }
 bootstrap();
