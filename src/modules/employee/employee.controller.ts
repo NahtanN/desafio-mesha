2a2ae6304940c01d46598b674dfba554e9a67dbe
NahtanN <nahtann@outlook.com>
2022-10-31 22:57:27 -0300

    feat(employee): rota para finalizar um atendimento

--- src/modules/employee/employee.controller.ts
+++ src/modules/employee/employee.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Param, Patch } from '@nestjs/common';
+import { Attendance, Prisma, TimeMeasures } from '@prisma/client';
 import { HttpResponse } from 'src/utils';
 import { AttendanceService } from '../attendance/attendance.service';
+import { EmployeeService } from './employee.service';
 
 @Controller('employee')
 export class EmployeeController {
-  constructor(private readonly attendanceService: AttendanceService) {}
+  constructor(
+    private readonly attendanceService: AttendanceService,
+    private readonly employeeService: EmployeeService,
+  ) {}
 
   @Patch('/attendance/start/:id')
   async startAttendance(@Param('id') id: string): Promise<HttpResponse> {
@@ -55,5 +60,64 @@ export class EmployeeController {
     );
 
     return HttpResponse.ok('Atendimento iniciado!', attendanceUpdated);
+  }
+
+  @Patch('/attendance/end/:id')
+  async endAttendance(@Param('id') id: string): Promise<HttpResponse> {
+    // TODO: Alterar
+    const employeeId = 1;
+
+    const attendanceId = parseInt(id);
+
+    const attendance: Partial<Attendance> = await this.attendanceService.find({
+      id: attendanceId,
+      employeeId,
+      startedIn: {
+        not: null,
+      },
+      endedIn: null,
+    });
+
+    if (!attendance) {
+      return HttpResponse.badRequest('Atendimento não encontrado!');
+    }
+
+    const timeMeasures = this.employeeService.calculateTimeElapsed(
+      attendance.startedIn,
+    );
+
+    const attendanceUpdated = await this.attendanceService.update(
+      {
+        id: attendanceId,
+      },
+      timeMeasures,
+      {
+        startedIn: true,
+        endedIn: true,
+        totalCommissionValue: true,
+        totalAttendanceTime: true,
+        attendanceTimeMeasure: true,
+        Client: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+        AttendanceServices: {
+          select: {
+            Service: {
+              select: {
+                name: true,
+                description: true,
+                estimatedTime: true,
+                timeMeasure: true,
+              },
+            },
+          },
+        },
+      },
+    );
+
+    return HttpResponse.ok('Atendimento finalizado!', attendanceUpdated);
   }
 }
--- src/modules/employee/employee.service.ts
+++ src/modules/employee/employee.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { TimeMeasures } from '@prisma/client';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  calculateTimeElapsed(startedIn: Date) {
+    const startedInDate = new Date(startedIn);
+    const endedIn = new Date();
+
+    let totalTime = endedIn.getTime() - startedInDate.getTime();
+
+    // Remove os milisegundos
+    totalTime /= 1000;
+
+    const seconds = Math.round(totalTime);
+
+    let totalAttendanceTime: number;
+    let attendanceTimeMeasure: TimeMeasures;
+
+    if (seconds / 60 <= 1) {
+      totalAttendanceTime = seconds;
+      attendanceTimeMeasure = 'SECONDS';
+    } else if (seconds / 60 >= 1 && seconds / 3600 < 1) {
+      totalAttendanceTime = Math.trunc(seconds / 60);
+      attendanceTimeMeasure = 'MINUTE';
+    } else if (seconds / 3600 >= 1 && seconds / 86400 < 1) {
+      totalAttendanceTime = Math.trunc(seconds / 3600);
+      attendanceTimeMeasure = 'HOUR';
+    } else {
+      totalAttendanceTime = Math.trunc(seconds / 86400);
+      attendanceTimeMeasure = 'DAY';
+    }
+
+    return {
+      endedIn,
+      totalAttendanceTime,
+      attendanceTimeMeasure,
+    };
+  }
+}
